10603d9939dc24fbdf397ef0f37b550b63cb505d
gustavo1902 <gustavomorais1009@gmail.com>
2023-08-17 09:22:35 -0300

    17/08

---
+++ src/maratona/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,27 @@
+package maratona.introducao;
+
+import java.util.Scanner;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu Nome: ");
+        String nome = scanner.nextLine(); // Lê uma linha inteira, incluindo espaços
+
+        System.out.print("Digite seu endereço: ");
+        String endereco = scanner.nextLine(); // Lê uma linha inteira
+
+        System.out.print("Digite o valor do salário recebido: ");
+        String salario = scanner.next();
+        scanner.nextLine(); // Consumir a nova linha pendente
+
+        System.out.print("Digite a data que recebeu o salário: ");
+        String data = scanner.next();
+
+        System.out.println("Eu: " + nome + ", morando no endereço " + endereco + ", confirmo que recebi o salário de " + salario + ", na data " + data);
+
+        scanner.close();
+    }
+}
+
