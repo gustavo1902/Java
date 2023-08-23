9ee6dbfd1b6e42c36b5d63f86e02ff5b774cc08f
gustavo1902 <gustavomorais1009@gmail.com>
2023-08-23 11:28:42 -0300

    atribuição

--- src/maratona/introducao/Aula04Operadores.java
+++ src/maratona/introducao/Aula04Operadores.java
@@ -1,10 +1,50 @@
 package maratona.introducao;
 
+import java.sql.SQLOutput;
+
 public class Aula04Operadores {
     public static void main(String[] args) {
         int numero01 = 10;
         int numero02 = 20;
         int resultado = numero02 / numero01;
         System.out.println("Valor "+ resultado);
+
+        // %
+        int resto = 20 % 2;
+        System.out.println(resto);
+
+        // < > <= => == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezDiferenteDez = 10 != 10;
+        System.out.println(isDezMaiorQueVinte);
+        System.out.println(isDezMenorQueVinte);
+        System.out.println(isDezIgualVinte);
+        System.out.println(isDezDiferenteDez);
+
+        // && (AND) || (OR) ! (NOT)
+        int idade = 35;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade > 30 && salario > 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+
+        System.out.println(isDentroDaLeiMaiorQueTrinta);
+        System.out.println(isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 5000F;
+        boolean isPlaystationCincoCompravel= valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
+        System.out.println(isPlaystationCincoCompravel);
+
+        // =, += -= *= /= %=
+        double bonus = 1800;
+        bonus += 1000;
+        System.out.println(bonus);
+        // ++
+        int contador = 1;
+        contador++;
+        System.out.println(contador);
     }
 }
