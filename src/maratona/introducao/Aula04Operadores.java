527ecf5ca1e28b83618e9ca7433f3e893d05e28b
gustavo1902 <gustavomorais1009@gmail.com>
2023-08-23 11:01:29 -0300

    operador

---
+++ src/maratona/introducao/Aula04Operadores.java
@@ -0,0 +1,10 @@
+package maratona.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        int numero01 = 10;
+        int numero02 = 20;
+        int resultado = numero02 / numero01;
+        System.out.println("Valor "+ resultado);
+    }
+}
