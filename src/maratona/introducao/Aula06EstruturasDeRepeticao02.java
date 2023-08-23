ef28b52585ed2eb0db7ad4752b196a0ebead21cc
gustavo1902 <gustavomorais1009@gmail.com>
2023-08-23 13:37:21 -0300

    repetição

---
+++ src/maratona/introducao/Aula06EstruturasDeRepeticao02.java
@@ -0,0 +1,14 @@
+package maratona.introducao;
+
+public class Aula06EstruturasDeRepeticao02 {
+    //Imprima os primeiros 25 números de um dado valor
+    public static void main(String[] args) {
+        int valorMax = 50;
+        for (int i=0; i<= valorMax ; i++){
+            if(i<=25){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
