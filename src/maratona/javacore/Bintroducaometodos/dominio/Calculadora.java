9eb540470fa806a55c7db730f5f0567bf8a4ae52
gustavo1902 <gustavomorais1009@gmail.com>
2023-08-24 14:34:37 -0300

    retorno

--- src/maratona/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/maratona/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -7,4 +7,10 @@ public class Calculadora {
     public void subtraiDoisNumeros(){
         System.out.println(10-5);
     }
+    public void multiplicaDoisNumeros(int num1, int num2){
+        System.out.println(num1*num2);
+    }
+    public double divideDoisNumeros(double num1, double num2){
+        return num1/num2;
+    }
 }
