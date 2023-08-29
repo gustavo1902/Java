cdf0f3f7c9bac26ee1c12567377212e49815f52c
gustavo1902 <gustavomorais1009@gmail.com>
2023-08-29 10:03:02 -0300

    herança

---
+++ src/maratona/javacore/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,15 @@
+package maratona.javacore.Hheranca.test;
+
+import maratona.javacore.Hheranca.dominio.Endereco;
+import maratona.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco("Rua 2", "012346-789");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Zeca");
+        pessoa.setCpf("111111111");
+        pessoa.setEndereco(endereco);
+    }
+}
+
