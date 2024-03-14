f11a42272c0999d9885c3fb53b9c0e5632d2af98
Gustavo <gustavo.morais@sou.unifal-mg.edu.br>
2024-03-14 13:11:47 -0300

    nothing

--- src/maratona/introducao/Aula06EstruturasDeRepeticao01.java
+++ src/maratona/introducao/Aula06EstruturasDeRepeticao01.java
@@ -4,9 +4,9 @@ public class Aula06EstruturasDeRepeticao01 {
     public static void main(String[] args) {
         // while, do while, for
         // Imprima todos os números pares de 0 até 100
-        for(int i=0; i<=100; i++){
-            if(i % 2 == 0)
-            System.out.println(i);
+        for(int i = 0; i<= 100; i++){
+            if(i%2==0)
+                System.out.println(i);
         }
     }
 }
