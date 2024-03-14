b313ece3ab38f0431d467658ce6d0c19283008e2
Gustavo <gustavo.morais@sou.unifal-mg.edu.br>
2024-03-14 12:53:51 -0300

    fix: add break case 6

--- src/maratona/introducao/Aula05EstruturasCondicionais05.java
+++ src/maratona/introducao/Aula05EstruturasCondicionais05.java
@@ -1,11 +1,14 @@
 package maratona.introducao;
 
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Aula05EstruturasCondicionais05 {
     public static void main(String[] args) {
         //Imprima o dia da semana, considerando 1 como domingo
-        byte dia = 5;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número entre 1 e 7 para representar o dia da semana: ");
+        byte dia = scanner.nextByte();
         // char, int, byte, short, enum, String
         switch (dia) {
             case 1:
@@ -25,12 +28,14 @@ public class Aula05EstruturasCondicionais05 {
                 break;
             case 6:
                 System.out.println("Sexta");
+                break;
             case 7:
                 System.out.println("Sábado");
                 break;
             default:
-                System.out.println("inválido");
+                System.out.println("Valor inválido, digite um número de 1 a 7.");
                 break;
         }
+        scanner.close();
     }
 }
