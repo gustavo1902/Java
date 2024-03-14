8cfb618adf463fa0405eb5f239533cc06b89c8b8
Gustavo <gustavo.morais@sou.unifal-mg.edu.br>
2024-03-14 13:03:32 -0300

    feat: add scanner

--- src/maratona/introducao/Aula05EstruturasCondicionais06.java
+++ src/maratona/introducao/Aula05EstruturasCondicionais06.java
@@ -1,9 +1,13 @@
 package maratona.introducao;
 
+import java.util.Scanner;
+
 public class Aula05EstruturasCondicionais06 {
     public static void main(String[] args) {
         // Dado os valores de 1 a 7, imprima se é dia útil ou final de semana
-        byte dia = 1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Insira o respectivo número para o dia da semana: ");
+        byte dia = scanner.nextByte();
         switch (dia){
             case 1:
             case 7:
