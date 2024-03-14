f6e65d0408da3c47ab338e956ad887fb2b807f35
Gustavo <gustavo.morais@sou.unifal-mg.edu.br>
2024-03-14 13:23:10 -0300

    feat: add break

--- src/maratona/introducao/Aula06EstruturasDeRepeticao02.java
+++ src/maratona/introducao/Aula06EstruturasDeRepeticao02.java
@@ -3,12 +3,12 @@ package maratona.introducao;
 public class Aula06EstruturasDeRepeticao02 {
     //Imprima os primeiros 25 números de um dado valor
     public static void main(String[] args) {
-        int valorMax = 50;
-        for (int i=0; i<= valorMax ; i++){
-            if(i<=25){
-                System.out.println(i);
-            }
-
-        }
+       int valorMax=50;
+       for (int i = 0; i<=valorMax; i++){
+           if(i>25) {
+               break;
+           }
+               System.out.println(i);
+       }
     }
 }
